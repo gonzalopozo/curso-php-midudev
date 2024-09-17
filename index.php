b6c03f44d7fda3eff1a058541dc327a7f6646ba0
gonzalopozo <gonzalopozo2005@gmail.com>
2024-09-17 19:39:26 +0200

    Estructura HTML

--- index.php
+++ index.php
@@ -15,7 +15,24 @@ curl_close($ch);
 var_dump($data);
 ?>
 
-<h2>La próxima película de Marvel</h2>
+<head>
+    <meta charset="UTF-8">
+    <title>La próxima película de Marvel</title>
+    <meta name="description" content="La próxima película de Marvel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
+    >
+</head>
+
+<main>
+    <section>
+        <img src="" alt="">
+        <h2>La próxima película de Marvel</h2>
+    </section>
+</main>
+
 
 
 
