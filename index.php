6a4f7600aff04a668a9a9fceeef4afcafcb05739
gonzalopozo <gonzalopozo2005@gmail.com>
2024-09-17 17:52:17 +0200

    PHP es Gradual (var_dump & gettype)

--- index.php
+++ index.php
@@ -3,12 +3,38 @@
     $isDev = true;
     $age = 19;
 
+    var_dump($name);
+    echo "<br>";
+    var_dump($isDev);
+    echo "<br>";
+    var_dump($age);
+
     $newAge1 = $age + '1';
     $newAge2 = $age . '1';
+    
+    echo "<br>";
+    var_dump($newAge1);
+    echo "<br>";
+    var_dump($newAge2);
+
+    echo "<br>";
+    echo "<br>";
+    echo gettype($name);
+    echo "<br>";
+    echo gettype($isDev);
+    echo "<br>";
+    echo gettype($age);
+    echo "<br>";
+
+    echo gettype($newAge1);
+    echo "<br>";
+    echo gettype($newAge2);
+    echo "<br>";
+
 ?>
 
 <h1>
-    <?= $newAge ?> 
+    <?= "Hola " . $name ?> 
 </h1>
 
 <style>
