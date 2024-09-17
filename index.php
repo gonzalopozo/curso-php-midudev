351966ae69f9afb85824f3ce322b44afb1358405
gonzalopozo <gonzalopozo2005@gmail.com>
2024-09-17 19:56:17 +0200

    Adición de Pico CSS && Creando estructura HTML, CSS y llamadas a la API

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $result = curl_exec($ch);
 $data = json_decode($result, true);
 curl_close($ch);
 
-var_dump($data);
+// var_dump($data);
 
 ### 2º Forma de llamar a una API --> Permite solo GET
 // $result = file_get_contents(API_URL);
@@ -32,17 +32,17 @@ var_dump($data);
 </head>
 
 <main>
+
     <section>
-        <img src="" alt="">
-        <h2>La próxima película de Marvel</h2>
+        <img src="<?= $data["poster_url"];?>" width="200" alt="Poster de <?= $data["title"]; ?>" style="border-radius: 16px;">
     </section>
+
+    <hgroup>
+        <h2><?= $data["title"]; ?> se estrena en <?= $data["days_until"]; ?> días</h2>
+        <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
+        <p>La siguiente película es <?= $data["following_production"]["title"]; ?></p>
+    </hgroup>
 </main>
-
-
-
-
-
-
 
 <style>
     :root {
@@ -52,5 +52,22 @@ var_dump($data);
     body {
         display: grid;
         place-content: center;
+    }
+
+    section {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+    }
+
+    hgroup {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+
+    img {
+        margin: 0 auto;
     }
 </style>
