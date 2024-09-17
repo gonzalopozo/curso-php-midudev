4fa94adbf6625b1e354407bc94727069b4c88737
gonzalopozo <gonzalopozo2005@gmail.com>
2024-09-17 19:35:14 +0200

    Inicio del proyecto de PHP && ¿Cómo llamar a un API?

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+const API_URL = "https://whenisthenextmcufilm.com/api";
+# Inicializar una nueva sesión de curl; ch = cURL handel
+$ch = curl_init(API_URL);
+// Indicar que queremos recibir el resultado de la petición y no mostrarla en pantalla
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+/*  Ejecutar la petición
+    y guardamos el resultado 
+*/
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+curl_close($ch);
+
+var_dump($data);
+?>
+
+<h2>La próxima película de Marvel</h2>
+
+
+
+
+
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
